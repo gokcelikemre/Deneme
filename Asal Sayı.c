f17a993c42cd79b68954aa9fe4ba76702a831125
gokcelikemre <57049722+gokcelikemre@users.noreply.github.com>
2020-05-15 17:38:32 +0300

    Add files via upload

---
+++ Asal Sayı.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+main()
+{
+	int i,sayi,sayac=0;
+	printf("Sayiyi girin : ");
+    scanf("%d",&sayi);
+    for(i=2;i<sayi;i++)
+    {
+       if(sayi%i==0)
+       {
+           sayac++;
+       }    
+    }  
+    if(sayac==0)
+    {
+        printf("Asal");                    
+    }
+    else
+    {
+        printf("Asal degil"); 
+    }
+
+ 
+}
